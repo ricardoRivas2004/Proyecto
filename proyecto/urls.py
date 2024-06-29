785294cecad24e04ee1811845e6dbcb19548c8be
ricardoRivas2004 <ricardorivasz2004@gmail.com>
2024-06-29 16:36:28 -0400

    django

---
+++ proyecto/urls.py
@@ -0,0 +1,15 @@
+#from django.conf.urls import url
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+
+    path('inicio', views.index, name='inicio'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('libro1', views.libro1, name='libro1'),
+    path('libro2', views.libro2, name='libro2'),
+    path('libro3', views.libro3, name='libro3'),
+]
---
+++ proyecto/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+
+    context={}
+
+    return render(request, 'proyecto/inicio.html', context)
+
+def login(request):
+
+    context={}
+
+    return render(request, 'proyecto/login.html', context)
+
+def register(request):
+
+    context={}
+
+    return render(request, 'proyecto/register.html', context)
+
+def libro1(request):
+
+    context={}
+
+    return render(request, 'proyecto/libro1.html', context)
+
+def libro2(request):
+
+    context={}
+
+    return render(request, 'proyecto/libro2.html', context)
+
+def libro3(request):
+
+    context={}
+
+    return render(request, 'proyecto/libro3.html', context)
