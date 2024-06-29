18c44b84f47d38c1109e4145c62d49c324f6e87c
ricardoRivas2004 <ri.rivas@duocuc.cl>
2024-06-29 16:46:56 -0400

    nuevo

--- js/register.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('.submit')
-const form   = document.querySelector('.form')
-
-button.addEventListener('click', function() {
-   form.classList.add('form--no') 
-});
